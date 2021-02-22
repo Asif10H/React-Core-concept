4e5c49623b987bb007f98f5a3010ad412068336e
Asif10H <habibullahasif71@gmail.com>
2021-02-23 01:14:41 +0600

    some html tag added

--- core-concepts/src/App.js
+++ core-concepts/src/App.js
@@ -3,9 +3,17 @@ import logo from './logo.svg';
 
 function App() {
   //var name = "Dr. Mahfuz";
-  var person = {
+  var person1 = {
     name: "Dr. Mahfuz",
     job: "singer"
+  }
+  var person2 = {
+    name: "Eva Rahman",
+    job: "Kokil Kondi"
+  }
+  var style={
+    color: 'red',
+    backgroundColor: 'yellow'
   }
   return (
     <div className="App">
@@ -15,9 +23,10 @@ function App() {
           Edit done <code>src/App.js</code> and save to reload.
         </p>
         <p>My first React Paragraph</p>
-        <p>My second React Paragraph</p>
+        <p style={style}>My second React Paragraph</p>
         <h2>my heading {44+33+3-66}</h2>
-        <h2>My heading {person.name +" "+ person.job}</h2>
+        <h2 className="" style={style}>My heading {person1.name +" "+ person1.job}</h2>
+        <h3 style={{ color: 'green',backgroundColor: 'tomato'}}>Singer: {person2.name +" "+ person2.job}</h3>
       </header>
     </div>
   );

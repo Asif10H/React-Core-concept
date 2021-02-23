fcfaa9e4a1f777d3fef3d285c47242dc62be3b00
Asif10H <habibullahasif71@gmail.com>
2021-02-23 20:19:45 +0600

    onMouseMove C

--- core-concepts/src/App.js
+++ core-concepts/src/App.js
@@ -60,7 +60,7 @@ function Counter(){
     <div>
       <h1>Counter : {count}</h1>
       
-      <button onClick = {handleIncrease}>Increase</button>
+      <button onMouseMove = {handleIncrease}>Increase</button>
       <button onClick = {() => setCount(count - 1)}>Decrease</button>
     </div>
   )
